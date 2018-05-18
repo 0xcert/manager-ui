c1008482e29c2c3f230af32920beded92e55f036
davidlicen <david@artnetik.si>
2018-05-18 13:48:47 +0200

    Add error screen

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@
       <StateReview v-if="$store.state.show.review" key="review" />
       <StateLoading v-if="$store.state.show.loading" key="loading" />
       <StateSuccess v-if="$store.state.show.success" key="success" />
+      <StateError v-if="$store.state.show.error" key="error" />
     </transition>
 
     <transition name="fade" mode="out-in">
@@ -27,6 +28,7 @@ import StateConfigure from '~/components/States/Configure'
 import StateReview from '~/components/States/Review'
 import StateSuccess from '~/components/States/Success'
 import StateLoading from '~/components/States/Loading'
+import StateError from '~/components/States/Error'
 
 export default {
   components: {
@@ -36,7 +38,8 @@ export default {
     StateConfigure,
     StateReview,
     StateSuccess,
-    StateLoading
+    StateLoading,
+    StateError
   }
 }
 </script>
